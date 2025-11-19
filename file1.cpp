daeab39e892c76baf65b2f18564f32031be6ca30
Ankita097 <ms.ankitaattri2004@gmail.com>
2025-11-19 21:16:20 +0530

    initialize first class

--- file1.cpp
+++ file1.cpp
@@ -2,12 +2,36 @@
 using namespace std;
 
 class Bus{
-     int id;
+     int bus_id;
      string bus_number;
      int seats_capacity; 
      int seats_availability;
      string driver_name;
      string route; 
+public:
+     Bus(int id, string bus_num, int capacity, int availability, string dri_name, string rout){
+          bus_id = id;
+          bus_number = bus_num;
+          seats_capacity = capacity;
+          seats_availability = availability;
+          driver_name = dri_name;
+          route = rout;
+      }
+
+      void getdata(){
+          cout << "Enter Bus Id: ";
+          cin >> bus_id;
+          cout << "The bus number is: ";
+          cin >> bus_number;
+          cout << "The bus capacity is: ";
+          cin >> seats_capacity;
+          cout << "The seat availability is: ";
+          cin >> seats_availability;
+          cout << "The driver name is: ";
+          cin >> driver_name;
+          cout << "The route is: ";
+          cin >> route;
+      }
 };
 
 class Route{
